bf7b6691976f9126c33f85cf3d3eb6235180dc91
lukasberk <lukasvdberk@gmail.com>
2021-05-26 15:33:46 +0200

    Cleaned up index.ts (connection in .env and added body parser)

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,34 @@
 import * as express from 'express';
 import * as dotenv from 'dotenv';
 import {routesManager} from "./routes/routesManager";
+import * as mongoose from 'mongoose';
+import * as bodyParser from 'body-parser';
 
 dotenv.config()
 
 const app = express();
+
 const port = process.env.PORT || 8080;
+
+mongoose.connect(process.env.MONGOOSE_CONN, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, () => {
+    console.log('connected to database!')
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/json' }));
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
+
+    next();
+});
 
 app.use(routesManager)
 
