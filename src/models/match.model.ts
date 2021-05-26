cd695eefcd1a21446747e9116403ab335ac50e81
Nils van der Velden <nilsepils123@gmail.com>
2021-05-26 15:29:30 +0200

    match model

---
+++ src/models/match.model.ts
@@ -0,0 +1,15 @@
+import * as mongoose from 'mongoose';
+
+const matchSchema = new mongoose.Schema({
+  internId: {
+    type: String,
+    required: true
+  },
+  companyId: {
+    type: String,
+    required: true
+  },
+})
+
+const Match = mongoose.model('Match', matchSchema)
+export { Match }
