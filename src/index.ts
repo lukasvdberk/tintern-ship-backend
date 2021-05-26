22e833f57f2fa15662aeedc7870cee2668a4325b
lukasberk <lukasvdberk@gmail.com>
2021-05-26 14:28:42 +0200

    Added port environment setup

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import * as express from 'express';
+import * as dotenv from 'dotenv';
+
+dotenv.config()
 
 const index = express();
-const port = 8080; // default port to listen
+const port = process.env.PORT || 8080;
 
-// define a route handler for the default home page
 index.get( "/", (req, res ) => {
     res.send( "Hello world!" );
 } );
 
-// start the Express server
 index.listen( port, () => {
     console.log( `server started at http://localhost:${ port }` );
 } );
