ebbeea166dcefe4efd93dd8dda6a6c926f77075a
Nils van der Velden <nilsepils123@gmail.com>
2021-05-26 15:36:10 +0200

    company & firebaseDeviceKey model

---
+++ src/models/firebaseDeviceKey.model.ts
@@ -0,0 +1,11 @@
+import * as mongoose from 'mongoose';
+
+const fireBaseDeviceKeySchema= new mongoose.Schema({
+  deviceKey: {
+    type: String,
+    required: true
+  }
+})
+
+const fireBaseDeviceKey = mongoose.model('fireBaseDeviceKey', fireBaseDeviceKeySchema)
+export { fireBaseDeviceKey }
