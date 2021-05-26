0a86d4f34227b9a4bdcd86a6c51d05db52c5b7a7
Nils van der Velden <nilsepils123@gmail.com>
2021-05-26 15:39:14 +0200

    intern model

---
+++ src/models/intern.mode.ts
@@ -0,0 +1,31 @@
+import * as mongoose from 'mongoose';
+
+const internSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true
+  },
+  educationId: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  phoneNumber: {
+    type: String,
+    required: true
+  }
+})
+
+const Intern = mongoose.model('Intern', internSchema)
+export { Intern }
