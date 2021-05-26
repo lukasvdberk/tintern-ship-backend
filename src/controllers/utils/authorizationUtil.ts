b734ae4dd993d431959d283581a4103476329f6e
lukasberk <lukasvdberk@gmail.com>
2021-05-26 15:06:36 +0200

    Added authorization setup

--- src/controllers/utils/authorizationUtil.ts
+++ src/controllers/utils/authorizationUtil.ts
@@ -1,31 +1,6 @@
 const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
 
 export class AuthorizationUtil {
-    /**
-     * Hahshes password with sha512.
-     * @function
-     * @param {string} password - The password you want to hash.
-     * @returns {Object} with {salt, passwordHash}
-     */
-    static async hashPassword(password) {
-        const randomLength = Math.random() * 16
-        const salt = bcrypt.genSaltSync(randomLength)
-
-        return bcrypt.hash(password, salt)
-    }
-
-    /**
-     * Checks is valid password
-     * @function
-     * @param {string} password - The password in plain text
-     * @param {string} hash - The hash you want to compare to the password (likely coming from a database)
-     * @returns {boolean} Success or not
-     */
-    static async validPassword(password, hash) {
-        return bcrypt.compare(password, hash)
-    }
-
     static getJWTKey() {
         return process.env.JWT_SECRET_KEY
     }
@@ -38,12 +13,10 @@ export class AuthorizationUtil {
      * @param {boolean} isAdmin - Wheather the user is admin or not
      * @returns {string} - The generated jwt token
      */
-    static createJWT(userId, email, isAdmin) {
-        const token = jwt.sign({userId, email: email, isAdmin}, this.getJWTKey(), {
+    static createJWT(userId, email, isAdmin): string {
+        return jwt.sign({userId, email: email, isAdmin}, this.getJWTKey(), {
             algorithm: 'HS256'
         })
-
-        return token
     }
 
     /**
@@ -52,7 +25,7 @@ export class AuthorizationUtil {
      * @param {Number} userId - User his id (likely coming from the database)
      * @returns {Object} - {userId,username,isAdmin} or undefined if the key is not valid
      */
-    static extractJWTInformation(jwtToken) {
+    static extractJWTInformation(jwtToken: string) {
         try {
             const payload = jwt.verify(jwtToken, this.getJWTKey(), {
                 algorithm: 'HS256'
@@ -61,7 +34,6 @@ export class AuthorizationUtil {
             return {
                 email: payload.email,
                 userId: payload.userId,
-                isAdmin: payload.isAdmin
             }
         } catch (exception) {
             return undefined
---
+++ src/controllers/utils/passwordUtil.ts
@@ -0,0 +1,28 @@
+import * as bcrypt from 'bcrypt';
+
+
+export class PasswordUtil {
+    /**
+     * Hashes password with sha512.
+     * @function
+     * @param {string} password - The password you want to hash.
+     * @returns {Object} with {salt, passwordHash}
+     */
+    static async hashPassword(password: string): Promise<string> {
+        const randomLength = Math.random() * 16
+        const salt = bcrypt.genSaltSync(randomLength)
+
+        return bcrypt.hash(password, salt)
+    }
+
+    /**
+     * Checks if a password is valid.
+     * @function
+     * @param {string} password - The password in plain text
+     * @param {string} hash - The hash you want to compare to the password (likely coming from a database)
+     * @returns {boolean} Success or not
+     */
+    static async validPassword(password, hash): Promise<boolean> {
+        return await bcrypt.compare(password, hash)
+    }
+}
