741c590903a0d35986316557389ee2993568d59a
Nils van der Velden <nilsepils123@gmail.com>
2021-05-29 11:09:54 +0200

    delete and edit intern

--- src/controllers/internController.ts
+++ src/controllers/internController.ts
@@ -14,14 +14,41 @@ export class InternController {
       description: intern.description,
       phoneNumber: intern.phoneNumber
     });
+
     await internDocument.save();
   
+    return ApiResponse.sendSuccessResponse(
+      {
+        intern: internDocument
+      }
+      ,res
+    );
+  }
+
+  static async editIntern(req, res, next) {
+
+    const internDocument = new Intern({
+      _id: req.body.id,
+      educationId: req.body.educationId,
+      name: req.body.name,
+      age: req.body.age,
+      description: req.body.description,
+      phoneNumber: req.body.phoneNumber
+    });
+    await internDocument.updateOne({ _id: req.params.id });
+
     return ApiResponse.sendSuccessResponse({}
       ,res
     );
   }
 
-  static async editIntern() {
+  static async deleteIntern(req, res, next) {
+    const internId = req.params.id;
+
+    await Intern.deleteOne({_id: internId})
     
+    return ApiResponse.sendSuccessResponse({}, 
+      res
+    );
   }
 }
