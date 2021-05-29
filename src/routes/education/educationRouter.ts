6496a6748f362cb352b9d3994a65beedc6c946e5
lukasberk <lukasvdberk@gmail.com>
2021-05-29 10:44:20 +0200

    Added education endpoint

--- src/routes/education/educationRouter.ts
+++ src/routes/education/educationRouter.ts
@@ -1,6 +1,7 @@
 import * as express from "express";
+import {EducationController} from "../../controllers/educationController";
 
 const educationRouter = express.Router();
-educationRouter.get("/educations");
+educationRouter.get("/educations", EducationController.getEducations);
 
 export { educationRouter };
