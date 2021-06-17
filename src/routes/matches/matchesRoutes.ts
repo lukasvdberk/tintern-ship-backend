17df94167b082eee943965944dda2b2be009b7fd
Nils van der Velden <nilsepils123@gmail.com>
2021-06-17 11:06:50 +0200

    :)

--- src/routes/matches/matchesRoutes.ts
+++ src/routes/matches/matchesRoutes.ts
@@ -1,6 +1,18 @@
 import * as express from "express";
+import { MatchController } from "../../controllers/matchController";
+import { AuthorizationMiddleware } from "../../middleware/authorization.middleware";
 
 const matchesRoutes = express.Router();
-matchesRoutes.get("/matches/user/:userId");
+  matchesRoutes.get(
+    "/matches/user/:userId",
+    AuthorizationMiddleware.isAuthenticated,
+    MatchController.getMatches
+  );
+
+  matchesRoutes.post(
+    "/matches/",
+    AuthorizationMiddleware.isAuthenticated,
+    MatchController.formMatch
+  );
 
 export { matchesRoutes };
